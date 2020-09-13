924b420382109df358c7a3662f92c7f93484fb38
Lucius <souzou@foxmail.com>
2020-09-13 14:01:24 +0800

    signup update

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -8,7 +8,7 @@
                     </b-input-group-prepend>
                     <b-form-input id="input-1" v-model="email" type="email" :state="emailState"></b-form-input>
                 </b-input-group>
-                <b-form-text>请使用清华邮箱</b-form-text>
+                <b-form-text v-if="emailError" style="color: red !important;">此邮箱已注册！</b-form-text>
             </b-form-group>
             <b-form-group label="密码" label-for="input-2" invalid-feedback="密码长度不足" :state="pwdState">
                 <b-input-group>
@@ -36,7 +36,7 @@
                 </b-input-group>
                 <b-form-text v-if="seen">两分钟后可再次发送</b-form-text>
             </b-form-group>
-            <b-button variant="outline-primary" id="signup-btn" @click.prevent="signup">注册</b-button>
+            <b-button variant="outline-primary" id="signup-btn" @click.prevent="signup" :disabled="cantSignup">注册</b-button>
         </b-form>
     </div>
 </template>
@@ -54,6 +54,8 @@ export default {
             tokenState: true,
             timer: null,
             seen: false,
+            emailError: false,
+            cantSignup: true
         }
     },
     computed: {
@@ -83,41 +85,49 @@ export default {
     },
     methods: {
         signup(){
-            this.onceTry = true
-            if(this.state){
-                this.axios.post('/api/signup', {
-                    email: this.email,
-                    password: this.password,
-                    token: this.token
-                }).then(response => {
-                    if(response.data.error === 'invalid token'){
-                        this.tokenState = false
-                    } else {
-                        if(this.timer !== null) clearInterval(this.timer);
-                        this.timer = null;
-                        this.$router.push('/login')
-                    }
-                })
-            }
+            this.axios.post('/api/signup', {
+                email: this.email,
+                password: this.password,
+                token: this.token
+            }).then(response => {
+                if(response.data.error === 'invalid token'){
+                    this.tokenState = false
+                } else {
+                    if(this.timer !== null) clearInterval(this.timer);
+                    this.timer = null;
+                    this.$router.push('/login')
+                }
+            })
         },
         change(evt){
-            this.axios.post('/api/signup', {
+            this.onceTry = true
+            if(this.state){
+                this.cantSignup = false
+                this.emailError = false
+                this.axios.post('/api/signup', {
                     email: this.email,
                     password: this.password
-            })
-            evt.target.innerText = '再次发送'
-            this.seen = true
-            if(!this.timer){
-                this.count = 120;
-                this.timer = setInterval(() => {
-                    if(this.count > 0){
-                        this.count--;
+                }).then(response => {
+                    if(response.data.error === 'email exists'){
+                        this.emailError = true
                     } else {
-                        clearInterval(this.timer);
-                        this.timer = null;
-                        this.seen = false;
+                        this.emailError = false
+                        evt.target.innerText = '再次发送'
+                        this.seen = true
+                        if(!this.timer){
+                            this.count = 120;
+                            this.timer = setInterval(() => {
+                                if(this.count > 0){
+                                    this.count--;
+                                } else {
+                                    clearInterval(this.timer);
+                                    this.timer = null;
+                                    this.seen = false;
+                                }
+                            }, 1000)
+                        }
                     }
-                }, 1000)
+                })
             }
         }
     }
